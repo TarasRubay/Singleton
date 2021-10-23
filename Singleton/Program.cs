3dcad740d67879aff6c7e32d522c20f0b232c86d
TarasRubay <84627325+TarasRubay@users.noreply.github.com>
2021-10-24 00:29:48 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton one = Singleton.Instans();
+            Singleton two = Singleton.Instans();
+            Console.WriteLine(ReferenceEquals(one,two));
+            two.SingeltonOperation();
+            Console.WriteLine(one.GetSingData());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        static Singleton uniqueInstans;
+        string singletonData = "";
+        protected Singleton() { }
+        public static Singleton Instans()
+        {
+            if (uniqueInstans == null)
+                uniqueInstans = new Singleton();
+            return uniqueInstans;
+        }
+        public void SingeltonOperation() {
+            singletonData = "SIng data";
+        }
+        public string GetSingData() {
+            return singletonData;
+        }
+    }
+}
